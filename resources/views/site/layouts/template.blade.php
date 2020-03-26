134b856a89f5e814ee4d9ebd0b86d3d9177b8fed
Reinaldo <nneetto885@hotmail.com>
2020-03-26 18:42:19 -0300

    text

---
+++ resources/views/site/layouts/template.blade.php
@@ -0,0 +1,373 @@
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Corporative a Corporate Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Corporative Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<!-- font-awesome icons -->
+<link rel="stylesheet" href="css/font-awesome.min.css" />
+<!-- //font-awesome icons -->
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="head-top-agileinfo">
+	<div class="header w3layouts">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.html">Corporative</a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="cl-effect-13" id="cl-effect-13">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="services.html">Services</a></li>
+							<li><a href="gallery.html">Gallery</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Short Codes <b class="caret"></b></a>
+								<ul class="dropdown-menu agile_short_dropdown">
+									<li><a href="icons.html">Icons</a></li>
+									<li><a href="typography.html">Typography</a></li>
+								</ul>
+							</li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+						
+					</nav>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- banner -->
+	<div class="w3-banner">
+		<div class="container">
+		<section class="slider">
+					<div class="flexslider">
+						<ul class="slides">
+							<li>
+							<div class="w3l-banner-info">
+									<h3>Lorem ipsum <br>Donec semper rutrum<br></h3>
+									<div class="agileinfo-news-button">
+										<a href="#" class="hvr-shutter-in-horizontal" data-toggle="modal" data-target="#myModal"> Read More</a>
+									</div>
+								</div>
+							<li>
+								<div class="w3l-banner-info">
+									<h3>condimentum<br>condimentum dolor velit<br></h3>
+									<div class="agileinfo-news-button">
+										<a href="#" class="hvr-shutter-in-horizontal" data-toggle="modal" data-target="#myModal"> Read More</a>
+									</div>
+								</div>							
+							</li>
+							<li>
+								<div class="w3l-banner-info">
+									<h3>Semper<br>Ipsum et bibendum<br></h3>
+									<div class="agileinfo-news-button">
+										<a href="#" class="hvr-shutter-in-horizontal" data-toggle="modal" data-target="#myModal"> Read More</a>
+									</div>
+								</div>							
+							</li>
+							<li>
+								<div class="w3l-banner-info">
+									<h3>Ipsum dolor <br>Donec semper rutrum<br></h3>
+									<div class="agileinfo-news-button">
+										<a href="#" class="hvr-shutter-in-horizontal" data-toggle="modal" data-target="#myModal"> Read More</a>
+									</div>
+								</div>						
+							</li>
+						</ul>
+					</div>
+				</section>
+			
+							<!-- FlexSlider -->
+									  <script defer="" src="js/jquery.flexslider.js"></script>
+									  <script type="text/javascript">
+										$(function(){
+										 
+										});
+										$(window).load(function(){
+										  $('.flexslider').flexslider({
+											animation: "slide",
+											start: function(slider){
+											  $('body').removeClass('loading');
+											}
+										  });
+										});
+									  </script>
+								<!-- FlexSlider -->
+		</div>
+	</div>
+	</div>
+<!-- //banner -->
+			
+
+<!-- services -->
+	<div class="services">
+		<div class="container">
+			<h2 class="w3l_head w3l_head1">What ?</h3>
+			<p class="w3ls_head_para w3ls_head_para2">Lorem ipsum dolor amet sit Donec semper rutrum  </p>
+			<p class="w3ls_head_para w3ls_head_para1">Lorem ipsum dolor amet sit Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit. Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit. Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit. Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit.</p>
+			<div class="w3_agileits_services_grids">
+				<div class="col-md-4 w3_agileits_services_grid">
+					<div class="w3_agileits_services_grid_agile">
+						<div class="w3_agileits_services_grid_1">
+							<i class="fa fa-map" aria-hidden="true"></i>
+						</div>
+						<h3>Rutrum ipsum  </h3>
+						<p>Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit.</p>
+					</div>
+				</div>
+				<div class="col-md-4 w3_agileits_services_grid">
+					<div class="w3_agileits_services_grid_agile">
+						<div class="w3_agileits_services_grid_1">
+							<i class="fa fa-globe" aria-hidden="true"></i>
+						</div>
+						<h3>Semper rutrum</h3>
+						<p>Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit.</p>
+					</div>
+				</div>
+				<div class="col-md-4 w3_agileits_services_grid">
+					<div class="w3_agileits_services_grid_agile">
+						<div class="w3_agileits_services_grid_1">
+							<i class="fa fa-gears" aria-hidden="true"></i>
+						</div>
+						<h3>Dolor velit </h3>
+						<p>Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit.</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //services -->
+<!-- branch -->
+	<div class="branch-wthree">
+		<div class="container">
+			<h3>Our branches</h3>
+			<p>Lorem ipsum dolor amet sit Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit. Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit. Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit. Donec semper rutrum ipsum et bibendum. Sed condimentum dolor velit.</p>
+			
+		</div>
+	</div>
+<!-- //branch -->
+<!-- team -->
+	<div class="team-agileits">
+		<div class="container">
+		<h3 class="w3l_head w3l_head1">Team</h3>
+			<p class="w3ls_head_para w3ls_head_para2">Lorem ipsum dolor amet sit Donec semper rutrum  </p>
+			<div class="team-grids">
+				<div class="col-md-4 team-grid">
+					<div class="pic">
+						<div class="stack twisted">	
+							<img src="images/4.jpg" alt=" " class="img-responsive" />
+						</div>
+						<div class="pic-caption top-to-bottom">
+							<h5 class="pic-title">Voluptate</h5>
+							<p>autem vel eum iure repreh qui in voluptate ea.</p>
+						</div>
+					</div>
+					<h4>Yang Li</h4>
+					<p>Departmental Head</p>
+					<ul class="social-nav model-3d-0">
+						  <li><a href="#" class="facebook">
+							  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="twitter"> 
+							  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="instagram">
+							  <div class="front"><i class="fa fa-dribbble" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-dribbble" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="pinterest">
+							  <div class="front"><i class="fa fa-vimeo" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-vimeo" aria-hidden="true"></i></div></a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 team-grid">
+					<div class="pic">
+						<div class="stack twisted">	
+							<img src="images/5.jpg" alt=" " class="img-responsive" />
+						</div>
+						<div class="pic-caption top-to-bottom">
+							<h5 class="pic-title">eumiure</h5>
+							<p>autem vel eum iure repreh qui in voluptate ea.</p>
+						</div>
+					</div>
+					<h4>Michael Li</h4>
+					<p>Manager</p>
+					<ul class="social-nav model-3d-0">
+						  <li><a href="#" class="facebook">
+							  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="twitter"> 
+							  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="instagram">
+							  <div class="front"><i class="fa fa-dribbble" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-dribbble" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="pinterest">
+							  <div class="front"><i class="fa fa-vimeo" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-vimeo" aria-hidden="true"></i></div></a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 team-grid">
+					<div class="pic">
+						<div class="stack twisted">	
+							<img src="images/7.jpg" alt=" " class="img-responsive" />
+						</div>
+						<div class="pic-caption top-to-bottom">
+							<h5 class="pic-title">reprehquin</h5>
+							<p>autem vel eum iure repreh qui in voluptate ea.</p>
+						</div>
+					</div>
+					<h4>Mark Carl</h4>
+					<p>Manager</p>
+					<ul class="social-nav model-3d-0">
+						  <li><a href="#" class="facebook">
+							  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="twitter"> 
+							  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="instagram">
+							  <div class="front"><i class="fa fa-dribbble" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-dribbble" aria-hidden="true"></i></div></a></li>
+						  <li><a href="#" class="pinterest">
+							  <div class="front"><i class="fa fa-vimeo" aria-hidden="true"></i></div>
+							  <div class="back"><i class="fa fa-vimeo" aria-hidden="true"></i></div></a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //team -->
+<!-- testimonial -->
+	<div class="testimonial agileinfo">
+		<div class="container">
+			<div class="about-heading testimonial-heading">
+				<h3>Testimonial</h3>
+			</div>
+			<div class="testimonial-grid">
+					<div class="slider">
+							<div class="callbacks_container">
+								<ul class="rslides" id="slider3">
+									<li>
+										<div class="agileits-w3layouts">
+											<i class="fa fa-quote-right" aria-hidden="true"></i>
+											<p>Donec feugiat tellus sem, laoreet iaculis orci lobortis vel. Sed sed luctus orci, at lacinia risus. Ut porttitor ante ac tincidunt elementum. Curabitur ex dolor, condimentum vitae nunc vel, pulvinar semper justo. Vestibulum et aliquet magna, maximus dapibus enim. Vestibulum ex lectus, posuere eu viverra at, mattis et enim. Nam efficitur sem et lectus fringilla, at pharetra nulla luctus. Nunc cursus, augue ac ultricies volutpat, neque erat congue justo, ac pretium tellus eros a neque. Integer ipsum sem, consectetur a mollis.</p>
+											<h5>Shaun <span>- Founder</span></h5>
+										</div>
+									</li>
+									<li>
+										<div class="agileits-w3layouts">
+											<i class="fa fa-quote-right" aria-hidden="true"></i>
+											<p>Pellentesque urna ex, ultricies a nunc at, pretium maximus nisi. Vestibulum non auctor diam. Mauris eget consectetur mauris. Aenean leo elit, accumsan vel elit vitae, mattis ultricies lacus. Cras consectetur justo lorem, sed dictum sapien eleifend at.Donec sed orci a dui aliquam tempor. Praesent in ipsum ut nunc porttitor lacinia.Donec eu sapien et arcu dictum rutrum.Ut laoreet vitae augue at accumsan. Nam pharetra sagittis purus et condimentum. Vestibulum cursus porttitor pretium.In egestas finibus rutrum. Nulla facilisi.</p>
+											<h5>Rogerer <span>- CEO</span></h5>
+										</div>
+									</li>
+								</ul>
+							</div>
+							<script>
+								// You can also use "$(window).load(function() {"
+								$(function () {
+								  // Slideshow 4
+								  $("#slider3").responsiveSlides({
+									auto: true,
+									pager:false,
+									nav:true,
+									speed: 500,
+									namespace: "callbacks",
+									before: function () {
+									  $('.events').append("<li>before event fired.</li>");
+									},
+									after: function () {
+									  $('.events').append("<li>after event fired.</li>");
+									}
+								  });
+							
+								});
+							 </script>
+							<!--banner Slider starts Here-->
+					</div>
+			</div>
+		</div>
+	</div>
+<!-- //testimonial -->
+<!-- footer -->
+	<div class="footer-w3-agile">
+		<div class="container">
+			<h2>Contact Us</h2>
+			<p class="w3ls_head_para w3ls_head_para2">Lorem ipsum dolor amet sit Donec semper rutrum  </p>
+		</div>
+	</div>
+<!-- //footer -->
+<!-- foot-agileits -->
+	<div class="foot-agileits-w3layouts">
+		<div class="container">
+			<div class="col-md-4 contact-info">
+				<div class="w3ls-cont">
+					<div class="address">	
+						<i class="glyphicon glyphicon-globe"></i>
+						<p class="p3">6th Lorem Street</p>
+						<p class="p3">Chicago, USA</p>
+					</div>
+					<div class="phone">
+						<i class="glyphicon glyphicon-phone-alt"></i>
+						<p class="p4">+00 1010101010</p>
+					</div>
+					<div class="email-info">
+						<i class="glyphicon glyphicon-envelope"></i>
+						<p class="p4"><a href="mailto:email2@example.com">email2@example.com</a></p>
+					</div>
+					<ul class="social-icons1">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-8 map-w3-agileits">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d190256.09869591036!2d-87.87223906433493!3d41.83364794910388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2c3cd0f4cbed%3A0xafe0a6ad09c0c000!2sChicago%2C+IL%2C+USA!5e0!3m2!1sen!2sin!4v1477387827354"  allowfullscreen></iframe>
+			</div>
+			<div class="clearfix"></div>
+			<div class="copyright-agileinfo">
+				<p>© 2017 Corporative. All Rights Reserved | Design by <a href="http://w3layouts.com" target="_blank">W3layouts</a></p>
+			</div>
+		</div>
+	</div>
+<!-- //foot-agileits -->
+<!-- for bootstrap working -->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<script src="js/responsiveslides.min.js"></script>
+
+</body>
+</html>
