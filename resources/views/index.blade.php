5c7c1b96dae4debfb0f1f2e1f35a7c8320a814ee
Reinaldo <nneetto885@hotmail.com>
2020-03-26 17:48:30 -0300

    subindo projeto - 26-03-20

---
+++ resources/views/index.blade.php
@@ -0,0 +1,81 @@
+<html>
+
+<link rel="stylesheet" href="../../public/css/aparte.css">
+<link rel="stylesheet" href="../../public/css/bootstrap.css">
+
+<body>
+
+<!-- 
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagens/carrossel2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/carrossel1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/carrossel3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> -->
+
+<div class="martop  text-center">
+
+<h3 class="text-center">A Empresa que Pensa em Seus Clientes</h3>
+<p>Somos uma empresa de terceirização de serviços de contabilidade, constituída por profissionais com larga experiência nas áreas em que atuamos: Contábil – Tributária – Fiscal e Trabalhista</p>
+<p>Orientando com base na legislação atualizada sobre o recolhimento de impostos e contribuições sociais. Nossa equipe está pronta para auxiliar as empresas no crescimento e fortalecimento administrativo financeiro em busca da maior qualidade, utilizando-se de sistemas e profissionais de alto nível.</p>
+<a href="about.php" class="btn p-2 btns">Ver mais</a>
+</div>
+<div class="bgray">
+<div class="martop">
+
+<h3 class="text-center">Serviços</h3>
+</div>
+<div class="row">
+    <div class="col-sm-3 text-center">
+        <img src="../../public/imagens/icontrabalho.png" alt="..." class="img-thumbnail imgservice">
+        <h5 >Trabalhista</h5>
+    
+    </div>
+    <div class="col-sm-3 text-center">
+        <img src="../../public/imagens/iconfiscal.png" alt="..." class="img-thumbnail imgservice">
+        <h5>Fiscal</h5>
+  
+    </div>
+    <div class="col-sm-3 text-center">
+        <img src="../../public/imagens/iconpessoal.png" alt="..." class="img-thumbnail imgservice">
+        <h5>Pessoal</h5>
+
+    </div>
+    <div class="col-sm-3 text-center">
+        <img src="../../public/imagens/iconcontabil.png" alt="..." class="img-thumbnail imgservice">
+        <h5>Contábil</h5>
+      
+    </div>
+
+</div>
+<div class="text-center">
+<a href="services.php" class="btn p-2 btns">Ver mais</a>
+</div>
+</div>
+
+<div class="martop text-center">
+
+<h3 class="text-center">Metodologia</h3>
+<p>O aspecto mais crítico do objetivo da contabilidade é o que constitui a informação “útil”. A fim de decidir isto, deve-se primeiramente identificar os usuários, para então determinar o que eles desejam saber. Os dados contábeis devem ser relevantes e transmitir confiabilidade para os seus leitores. Diante deste impasse, conclui-se que o Conjunto das Demonstrações Financeiras é um instrumento de extrema valia na análise de uma organização, de um empreendimento</p>
+<a href="services.php" class="btn p-2 btns">Ver mais</a>
+</div>
+
+
+</body>
+
+</html>
---
+++ resources/views/services.blade.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="../public/css/aparte.css">
+<link rel="stylesheet" href="../public/css/bootstrap.css">
+<div class="bgray">
+<h3 class="text-center">Serviços</h3>
+</div>
+<div class="row text-center">
+    <div class="col-sm-4">
+    <img src="../../public/imagens/iconcontabil.png" class="img-thumbnail" alt="...">
+    <h5>CONTÁBIL</h5>
+    <p>Por meio da contabilidade, nossos técnicos prestam assessoria estratégica nos negócios das empresas, traçando análises financeiras que permitem ao empresário tomar decisões seguras.</p>
+    </div>
+    <div class="col-sm-4">
+    <img src="../../public/imagens/iconfiscal.png" class="img-thumbnail" alt="...">
+    <h5>FISCAL</h5>
+    <p>A carga tributária tem grande relevância nos negócios das empresas, por isso, notificamos de todas as informações e fornecemos todos os direcionamentos fiscais dos nossos clientes. </p>
+    </div>
+    <div class="col-sm-4">
+    <img src="../../public/imagens/icontrabalho.png" class="img-thumbnail" alt="...">
+    <h5>TRABALHISTA</h5>
+    <p>Por meio desta área, imprescindível para uma empresa, executamos toda a rotina do departamento pessoal da instituição</p>   
+    </div>
+    </div>
+    <div class="row text-center bgray p-3">
+
+    <div class="col-sm-4">
+    <img src="../../public/imagens/iconpessoal.png" class="img-thumbnail" alt="...">
+    <h5>PESSOAL</h5>
+    <p>Elaboração de todos os serviços pessoais solicitados, com garantia de execução primorosa, respeitando os prazos determinados, o sigilo das informações e a ética profissional</p>
+    </div>
+    <div class="col-sm-4">
+    <img src="../../public/imagens/icontributaria.png" class="img-thumbnail" alt="...">
+    <h5>TRIBUTÁRIA</h5>
+    <p>Por meio de análises e estudos elaborados por esta área, nossos clientes conseguem reduzir os custos fiscais, permitindo uma melhor formação de preço e maior rentabilidade no mercado em que atuam. </p>   
+    </div>
+    <div class="col-sm-4">
+    <img src="../../public/imagens/iconlegislacao.png" class="img-thumbnail" alt="...">
+    <h5>LEGISLAÇÃO</h5>
+    <p>Assessoramos por todas as atividades legais, tais como: abertura, alteração e baixa de empresas; elaboração, alteração e adequação de Contratos Sociais e Distrato e outros.</p>   
+    </div>
+    
+</div>
